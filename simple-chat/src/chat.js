cf80940c66a5d9542177e40233370cbe4b3889e7
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-11-01 00:35:14 +0300

    Update chat.js

--- simple-chat/src/chat.js
+++ simple-chat/src/chat.js
@@ -1,4 +1,5 @@
 import './index.css';
-import './css/display.css'
-import './css/form.css'
-import './js/rendering_messages.js'
+import './css/display.css';
+import './css/form.css';
+import './css/cap.css';
+import './js/rendering_messages.js';
