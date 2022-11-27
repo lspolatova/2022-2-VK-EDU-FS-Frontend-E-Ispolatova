8699baefb4b3962fd13ccf32e6037e19fca4abeb
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-11-28 02:47:27 +0300

    Update List_chats.jsx

--- react-chat/src/components/List_chats/List_chats.jsx
+++ react-chat/src/components/List_chats/List_chats.jsx
@@ -1,14 +1,21 @@
 import { Chat } from "../";
 import "./List_chats.scss";
 import { MaxIndex } from "../../utils";
-
+import React, { useEffect, useRef } from 'react'
 
 export function List_chats(props) {
+    const chatEndRef = useRef(null)
     let max_index = new MaxIndex();
     let chat = [];
     let last_message;
     let last_time;
     let colvo = (parseInt(max_index.index) + 1).toString();
+    const scrollToBottom = () => {
+        chatEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+    useEffect(() => {
+        scrollToBottom()
+    }, [chat]);
     if (localStorage.getItem(max_index.index)){
         last_message = JSON.parse(localStorage.getItem(max_index.index)).message;
         last_time = JSON.parse(localStorage.getItem(max_index.index)).time_loc;
@@ -26,6 +33,7 @@ export function List_chats(props) {
     return(
         <div className="list_chats">
             {chat}
+            <div ref={chatEndRef} />
         </div>
     );
 }
