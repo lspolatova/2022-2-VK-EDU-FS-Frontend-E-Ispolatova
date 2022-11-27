69e966e5b22b41e363fa95173218af1e4fa9876d
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-11-28 02:21:09 +0300

    Update App.js

--- react-chat/src/App.js
+++ react-chat/src/App.js
@@ -8,18 +8,24 @@ class App extends React.Component {
             page: 'chat_list'
         };
         this.changePage = this.changePage.bind(this)
+        this.changePage_back = this.changePage_back.bind(this)
     }
     changePage() {
       this.setState({
         page: 'chat'
       })
     }
+    changePage_back() {
+        this.setState({
+          page: 'chat_list'
+        })
+    }
 
     render() {
         if(this.state.page === 'chat_list'){
             return(<Chat_list changePage = {this.changePage}/>)
         }else{
-            return(<ChatPage/>)
+            return(<ChatPage changePage = {this.changePage_back}/>)
         }
     }
 }
