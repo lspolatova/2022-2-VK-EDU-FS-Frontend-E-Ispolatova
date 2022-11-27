d8a899029219d1d758b842be55edf854f4ee24b7
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-11-28 02:10:03 +0300

    Update Back.jsx

--- react-chat/src/components/Back/Back.jsx
+++ react-chat/src/components/Back/Back.jsx
@@ -1,10 +1,6 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-function test_click_event()
-{
-     alert("Button clicked");
-}
 export function Back(props) {
     return (
-        <ArrowBackIcon onClick={test_click_event}/>
+        <ArrowBackIcon onClick={props.changePage}/>
     );
 }
