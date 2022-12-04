de8b7efbb1661c9e45c4dfa9e9882af6219e7a5a
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-12-04 17:53:10 +0300

    Add files via upload

---
+++ react-chat/src/components/Display/Display.jsx
@@ -0,0 +1,31 @@
+import "./Display.scss";
+import "../Chat/Chat.scss";
+import { Message } from "../Message";
+import React, { useEffect, useRef } from 'react'
+
+export function Display(props) {
+    let index_speech = 0;
+    let messages = [];
+    const messagesEndRef = useRef(null)
+    
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages]);
+
+    while(index_speech <= parseInt(props.index_max)){
+        messages.push(<Message key = {index_speech}
+            message = {JSON.parse(localStorage.getItem(`${index_speech}_${props.name}`)).message}
+            time_loc = {JSON.parse(localStorage.getItem(`${index_speech}_${props.name}`)).time_loc}
+        />);
+        ++index_speech;
+    }
+    return(
+        <div className="display">
+            {messages}
+            <div ref={messagesEndRef} />
+        </div>
+    );
+}
---
+++ react-chat/src/components/photo/photo.jsx
@@ -0,0 +1,5 @@
+import "./photo.scss"
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+export const photo = (
+	<CameraAltIcon id = "photo"/>
+);
