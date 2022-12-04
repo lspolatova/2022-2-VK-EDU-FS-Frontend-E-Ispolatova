9828e6fac35d60e75493b0380f1ae9eca0cc921e
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-12-04 17:50:26 +0300

    Delete react-chat/src directory

--- react-chat/src/components/Back/Back.jsx
+++
@@ -1,6 +0,0 @@
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-export function Back(props) {
-    return (
-        <ArrowBackIcon onClick={props.changePage}/>
-    );
-}
--- react-chat/src/components/edit/edit.jsx
+++
@@ -1,5 +0,0 @@
-import "./edit.scss"
-import EditIcon from '@mui/icons-material/Edit';
-export const edit = (
-	<EditIcon id = "edit"/>
-);
