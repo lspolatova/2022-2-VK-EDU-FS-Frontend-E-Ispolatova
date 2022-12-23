1ead9acac2e7d0b2b02e91876f0078b6f6d97021
lspolatova <34059354+lspolatova@users.noreply.github.com>
2022-12-23 20:30:51 +0300

    Delete react-chat/src directory

--- react-chat/src/components/Back/Back.jsx
+++
@@ -1,9 +0,0 @@
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import {Link} from 'react-router-dom'
-export function Back(props) {
-    return (
-        <Link to={`/`} style={{ textDecoration: 'none' , color: 'white', zoom: '1.5', padding: '0.5rem', height: '10px'}}>
-            <ArrowBackIcon/>
-        </Link>
-    );
-}
--- react-chat/src/components/Form_profile/index.js
+++
@@ -1,1 +0,0 @@
-export * from "./Form_profile.jsx"
